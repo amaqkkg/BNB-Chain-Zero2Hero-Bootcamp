c4eba5facea956867da9de5f69b2edb0cae11548
0xC4111B4H <faris@MacBook-Pro.local>
2023-03-10 05:10:54 +0700

    update lesson04

---
+++ lesson04/DogCoin.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+contract DogCoin {
+    uint256 totalSupply;
+    mapping(address => uint256) public balances;
+    struct Payment {
+        uint256 _amount;
+        address _recipient;
+    }
+
+    mapping(address => Payment[]) public historyTx;
+
+    event SupplyChanged(uint256);
+    event TokenTransferred(uint256, address);
+
+    function getTotalSupply() public view returns (uint256) {
+        return totalSupply;
+    }
+
+    function increaseSupply(uint256 _step) public onlyOwner {
+        totalSupply += _step * 1000;
+        emit SupplyChanged(totalSupply);
+    }
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+        totalSupply = 2000000;
+        balances[msg.sender] = totalSupply;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    // we dont need sender address we check it in the next line.
+    // if we set the sender address as parameter then everyone can transfer anyone token balances.
+    function transfer(uint256 _amount, address _recipient) public {
+        require(balances[msg.sender] >= _amount, "not enough balance!");
+        balances[msg.sender] -= _amount;
+        balances[_recipient] += _amount;
+        historyTx[msg.sender].push(Payment(_amount, _recipient));
+        emit TokenTransferred(_amount, _recipient);
+    }
+}
